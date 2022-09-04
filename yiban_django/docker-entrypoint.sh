f7dbfd33c5d3e2a8192caa061fdbec4dba476e53
Holmose <53456573+Holmose@users.noreply.github.com>
2022-09-04 12:29:13 +0800

    Update docker-entrypoint.sh

--- yiban_django/docker-entrypoint.sh
+++ yiban_django/docker-entrypoint.sh
@@ -4,10 +4,6 @@ set -e
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata admin_interface_theme_django.json
-python manage.py loaddata admin_interface_theme_bootstrap.json
-python manage.py loaddata admin_interface_theme_foundation.json
-python manage.py loaddata admin_interface_theme_uswds.json
 
 /usr/local/bin/uwsgi --ini uwsgi.ini
 
