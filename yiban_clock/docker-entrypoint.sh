db2ee32e0d091d7f1c4cc33d67860009a54192e3
holmose <holmose@qq.com>
2022-09-17 09:27:19 +0800

    将邮件配置放到.env中进行

--- yiban_clock/docker-entrypoint.sh
+++ yiban_clock/docker-entrypoint.sh
@@ -22,7 +22,18 @@ if [ ! $MYSQL_DATABASE ];then
   echo "The environment variable MYSQL_USER empty"
   exit 100
 fi
-
+if [ ! $MAIL_USER ];then
+  echo "The environment variable MYSQL_USER empty"
+  exit 100
+fi
+if [ ! $MAIL_PASS ];then
+  echo "The environment variable MYSQL_USER empty"
+  exit 100
+fi
+if [ ! $MAIL_HOST ];then
+  echo "The environment variable MYSQL_USER empty"
+  exit 100
+fi
 if [ ! -f /app/config ];then
   mkdir -p /app/config
   cp /config.json /app/config/config.json
@@ -31,6 +42,10 @@ if [ ! -f /app/config ];then
   sed -i 's/$MYSQL_HOST/'$MYSQL_HOST'/g' /app/config/config.json
   sed -i 's/$MYSQL_PORT/'$MYSQL_PORT'/g' /app/config/config.json
   sed -i 's/$MYSQL_DATABASE/'$MYSQL_DATABASE'/g' /app/config/config.json
+  
+  sed -i 's/$MAIL_USER/'$MAIL_USER'/g' /app/config/config.json
+  sed -i 's/$MAIL_PASS/'$MAIL_PASS'/g' /app/config/config.json
+  sed -i 's/$MAIL_HOST/'$MAIL_HOST'/g' /app/config/config.json
 fi
 set +e
 
